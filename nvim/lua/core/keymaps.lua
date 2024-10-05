6fdc7ae2d57dcc4c44d87b39162dc97e84b775e3
matteo <matteo.sepa@proton.me>
2024-10-05 21:36:22 +0200

    updated keymaps with neotree keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -24,3 +24,7 @@ vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
 vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
 vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
 vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+
+
+-- Neotree keymap
+vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>')
